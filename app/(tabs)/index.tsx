c968512cbe0b4eda4ef62c3ac0a92889ebff0cf2
Ru1ean <javellanavja@gmail.com>
2026-01-31 02:26:36 +0800

    initial

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,7 +18,7 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Welcome gwapo or should i say "most fine looking southeast asian stud"</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
